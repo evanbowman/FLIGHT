a6301ad5526c88cc5ab761c4b87ec1f9a0652f3f
evanbowman <ebowman@bu.edu>
2017-01-30 22:13:02 -0500

    added exe path function

---
+++ src/ResourcePath.cpp
@@ -0,0 +1,37 @@
+#include "ResourcePath.hpp"
+
+#ifdef FLIGHT_WINDOWS
+std::string ResourcePath() {
+    HMODULE hModule = GetModuleHandleW(nullptr);
+    char buffer[MAX_PATH];
+    GetModuleFileName(hModule, buffer, MAX_PATH);
+    const std::string path(buffer);
+    const std::size_t lastFwdSlash = path.find_last_of('\\');
+    std::string pathWithoutBinary = path.substr(0, lastFwdSlash + 1);
+#ifdef _DEBUG
+    return pathWithoutBinary + "..\\..\\res\\";
+#else
+    return pathWithoutBinary + "res\\";
+#endif
+}
+#elif FLIGHT_MAC
+std::string ResourcePath() {
+    char buffer[PATH_MAX];
+    uint32_t size = sizeof(buffer);
+    _NSGetExecutablePath(buffer, &size);
+    const std::string path(buffer);
+    const std::size_t lastFwdSlash = path.find_last_of("/");
+    std::string pathWithoutBinary = path.substr(0, lastFwdSlash + 1);
+    return pathWithoutBinary + "../";
+}
+#elif FLIGHT_LINUX
+std::string ResourcePath() {
+    char buffer[PATH_MAX];
+    [[gnu::unused]] const std::size_t bytesRead =
+        readlink("/proc/self/exe", buffer, sizeof(buffer));
+    const std::string path(buffer);
+    const std::size_t lastFwdSlash = path.find_last_of("/");
+    std::string pathWithoutBinary = path.substr(0, lastFwdSlash + 1);
+    return pathWithoutBinary + "../res/";
+}
+#endif
---
+++ src/ResourcePath.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "config.h"
+
+#include <cstring>
+#include <string>
+#ifdef BLINDJUMP_WINDOWS
+#include <Windows.h>
+#elif BLINDJUMP_MAC
+#include <mach-o/dyld.h>
+#include <sys/syslimits.h>
+#elif BLINDJUMP_LINUX
+#include <linux/limits.h>
+#include <unistd.h>
+#endif
+
+std::string ResourcePath();
